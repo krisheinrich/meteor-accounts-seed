88d20d0bbf66ae0c620394bbd339556abe79d586
krisheinrich <krisheinrich@gmail.com>
2016-08-02 22:31:52 -0700

    Added user accounts support (username/password and Google authentication), database docs, CSS syles, frontend UI with CRUD operations, reactively updating Angular components

--- imports/components/todosList/todosList.js
+++ imports/components/todosList/todosList.js
@@ -1,16 +1,71 @@
 import angular from 'angular';
 import angularMeteor from 'angular-meteor';
+import { Meteor } from 'meteor/meteor';
+import { Tasks } from '../../api/tasks.js';
+
 import template from './todosList.html';
 
 class TodosListCtrl {
-  constructor() {
-    this.tasks = [{
-      text: 'This is task 1'
-    }, {
-      text: 'This is task 2'
-    }, {
-      text: 'This is task 3'
-    }];
+  constructor($scope) {
+    $scope.viewModel(this);
+
+    this.hideCompleted = false;
+
+    this.helpers({
+      tasks() {
+        const selector = {};
+
+        // If hide completed is checked, filter tasks
+        if (this.getReactively('hideCompleted')) {
+          selector.checked = {
+            $ne: true
+          };
+        }
+
+        // Show newest tasks at the top
+        return Tasks.find(selector, {
+          sort: {
+            createdAt: -1
+          }
+        });
+      },
+      incompleteCount() {
+        return Tasks.find({
+          checked: {
+            $ne: true
+          }
+        }).count();
+      },
+      currentUser() {
+        return Meteor.user();
+      }
+    });
+  }
+
+  addTask(newTask) {
+    // Insert a task into the collection
+    Tasks.insert({
+      text: newTask,
+      createdAt: new Date(),
+      owner: Meteor.userId(),
+      username: Meteor.user().username
+    });
+
+    // Clear form
+    this.newTask = '';
+  }
+
+  setChecked(task) {
+    // Set the checked property to the opposite of its current value
+    Tasks.update(task._id, {
+      $set: {
+        checked: !task.checked
+      },
+    });
+  }
+
+  removeTask(task) {
+    Tasks.remove(task._id);
   }
 }
 
@@ -19,5 +74,5 @@ export default angular.module('todosList', [
 ])
   .component('todosList', {
     templateUrl: 'imports/components/todosList/todosList.html',
-    controller: TodosListCtrl
+    controller: ['$scope', TodosListCtrl]
   });
